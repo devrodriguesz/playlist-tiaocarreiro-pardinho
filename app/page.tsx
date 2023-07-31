367900db2feb34fa47d36f5672ad86d396ea729b
devrodriguesz <dev.emersonrodrigues@outlook.com>
2023-07-30 22:37:38 -0300

    feat: implementar método get para recuperar informações do álbum na API

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './page.module.css'
 import TextField from '@mui/material/TextField';
@@ -12,9 +12,34 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { DataGrid, GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
 
 import Header from './components/Header/page'
+import getAlbum from './services/api';
+
+interface Item {
+  name: string;
+  year: number;
+  id: number;
+  tracks: Track[];
+}
+
+interface Track {
+  id: number;
+  number: number;
+  title: string;
+  duration: number;
+}
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
+  const [dataPlaylist, setDataPlaylist] = useState([])
+
+  useEffect(() => {
+    async function fetchAlbum() {
+      const response = await getAlbum('album');
+      setDataPlaylist(response.data);
+    }
+
+    fetchAlbum();
+  }, []);
 
   const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     setInputValue(event.target.value);
@@ -38,7 +63,7 @@ export default function Home() {
 
   const columns: GridColDef[] = [
     { field: 'number', headerName: 'Nº', width: 70, disableColumnMenu: true },
-    { field: 'track', headerName: 'Faixa', width: 150, disableColumnMenu: true },
+    { field: 'title', headerName: 'Faixa', width: 150, disableColumnMenu: true },
     { field: 'duration', headerName: 'Duração', width: 130, disableColumnMenu: true, sortable: false },
     { field: 'spacer', headerName: '', width: 600, disableColumnMenu: true, sortable: false },
     {
@@ -52,28 +77,28 @@ export default function Home() {
   ];
 
   const rows = [
-    { id: 1, number: 1, track: 'Minas Gerais', duration: '03:26' },
-    { id: 2, number: 2, track: 'A Gerais', duration: '99:99' },
-    { id: 3, number: 3, track: 'Minas Gerais', duration: '03:26' },
-    { id: 4, number: 4, track: 'A Gerais', duration: '99:99' },
-    { id: 5, number: 5, track: 'Minas Gerais', duration: '03:26' },
-    { id: 6, number: 6, track: 'A Gerais', duration: '99:99' },
-    { id: 7, number: 7, track: 'Minas Gerais', duration: '03:26' },
-    { id: 8, number: 8, track: 'A Gerais', duration: '99:99' },
-    { id: 9, number: 9, track: 'Minas Gerais', duration: '03:26' },
-    { id: 10, number: 10, track: 'A Gerais', duration: '99:99' },
-    { id: 11, number: 11, track: 'Minas Gerais', duration: '03:26' },
-    { id: 12, number: 12, track: 'A Gerais', duration: '99:99' },
-    { id: 13, number: 13, track: 'Minas Gerais', duration: '03:26' },
-    { id: 14, number: 14, track: 'A Gerais', duration: '99:99' },
-    { id: 15, number: 15, track: 'Minas Gerais', duration: '03:26' },
-    { id: 16, number: 16, track: 'A Gerais', duration: '99:99' },
-    { id: 17, number: 17, track: 'Minas Gerais', duration: '03:26' },
-    { id: 18, number: 18, track: 'A Gerais', duration: '99:99' },
-    { id: 19, number: 19, track: 'Minas Gerais', duration: '03:26' },
-    { id: 20, number: 20, track: 'A Gerais', duration: '99:99' },
-    { id: 21, number: 21, track: 'Minas Gerais', duration: '03:26' },
-    { id: 22, number: 22, track: 'A Gerais', duration: '99:99' },
+    { id: 1, number: 1, title: 'Minas Gerais', duration: '03:26' },
+    { id: 2, number: 2, title: 'A Gerais', duration: '99:99' },
+    { id: 3, number: 3, title: 'Minas Gerais', duration: '03:26' },
+    { id: 4, number: 4, title: 'A Gerais', duration: '99:99' },
+    { id: 5, number: 5, title: 'Minas Gerais', duration: '03:26' },
+    { id: 6, number: 6, title: 'A Gerais', duration: '99:99' },
+    { id: 7, number: 7, title: 'Minas Gerais', duration: '03:26' },
+    { id: 8, number: 8, title: 'A Gerais', duration: '99:99' },
+    { id: 9, number: 9, title: 'Minas Gerais', duration: '03:26' },
+    { id: 10, number: 10, title: 'A Gerais', duration: '99:99' },
+    { id: 11, number: 11, title: 'Minas Gerais', duration: '03:26' },
+    { id: 12, number: 12, title: 'A Gerais', duration: '99:99' },
+    { id: 13, number: 13, title: 'Minas Gerais', duration: '03:26' },
+    { id: 14, number: 14, title: 'A Gerais', duration: '99:99' },
+    { id: 15, number: 15, title: 'Minas Gerais', duration: '03:26' },
+    { id: 16, number: 16, title: 'A Gerais', duration: '99:99' },
+    { id: 17, number: 17, title: 'Minas Gerais', duration: '03:26' },
+    { id: 18, number: 18, title: 'A Gerais', duration: '99:99' },
+    { id: 19, number: 19, title: 'Minas Gerais', duration: '03:26' },
+    { id: 20, number: 20, title: 'A Gerais', duration: '99:99' },
+    { id: 21, number: 21, title: 'Minas Gerais', duration: '03:26' },
+    { id: 22, number: 22, title: 'A Gerais', duration: '99:99' },
   ];
 
 
@@ -87,7 +112,7 @@ export default function Home() {
             size="large"
             endIcon={<AddCircleIcon />}
             onClick={createPlaylist}
-            >              
+          >
             Criar Playlist
           </Button>
 
@@ -121,42 +146,51 @@ export default function Home() {
           />
         </div>
 
-        <div className={styles.containerItem}>
-          <div className={styles.playlistHeader}>
-            <div className={styles.playlistTitle}>
-              <h2>Rei do Gado, 1961</h2>
-              <Button
-                variant="outlined"
-                startIcon={<AddCircleIcon />}
-                sx={{
-                  fontWeight: 700,
+        <div>
+          {dataPlaylist.map((item: Item) => (
+            <div key={item.id} className={styles.containerItem}>
+              <div className={styles.playlistHeader}>
+                <div className={styles.playlistTitle}>
+                  <h2>{item.name}, {item.year}</h2>
+                  <Button
+                    variant="outlined"
+                    startIcon={<AddCircleIcon />}
+                    sx={{
+                      fontWeight: 700,
+                    }}
+                    onClick={addTrack}
+                  >
+                    Adicionar Música</Button>
+                </div>
+                <Button
+                  variant="outlined"
+                  color='error'
+                  startIcon={<DeleteIcon />}
+                  sx={{
+                    fontWeight: 700,
+                  }}
+                  onClick={deletePlaylist}
+                >
+                  Deletar Playlist</Button>
+              </div>
+              <DataGrid
+                rows={item.tracks.map((track: Track) => {
+                  const minutes = Math.floor(track.duration / 60);
+                  const seconds = track.duration % 60;
+                  const formattedDuration = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+                  return { ...track, duration: formattedDuration };
+                })}
+                columns={columns}
+                rowSelectionModel={[] as GridRowSelectionModel}
+                initialState={{
+                  pagination: {
+                    paginationModel: { page: 0, pageSize: 5 },
+                  },
                 }}
-                onClick={addTrack}
-              >
-                Adicionar Música</Button>
+                pageSizeOptions={[5, 10, 15, 20, 50, 100]}
+              />
             </div>
-            <Button
-              variant="outlined"
-              color='error'
-              startIcon={<DeleteIcon />}
-              sx={{
-                fontWeight: 700,
-              }}
-              onClick={deletePlaylist}
-            >
-              Deletar Playlist</Button>
-          </div>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            rowSelectionModel={[] as GridRowSelectionModel}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            pageSizeOptions={[5, 10, 15, 20, 50, 100]}
-          />
+          ))}
         </div>
 
       </div>
--- app/services/api.tsx
+++ app/services/api.tsx
@@ -1,17 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const apiPlaylist = axios.create({
   baseURL: 'https://tiao.supliu.com.br/api/',
-})
+});
 
-const playlist = async (url: string) => {
-  try {
-    const response = await apiPlaylist.get(url)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    return null
+apiPlaylist.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = 'dev.emersonrodrigues@outlook.com';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-}
+);
 
-export default playlist
+const getAlbum = async (url: string) => {
+  try {
+    const response = await apiPlaylist.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getAlbum;
